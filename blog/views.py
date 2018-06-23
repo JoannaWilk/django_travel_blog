38524220b2c126b56b5912af178b2359026f4b89
JoannaWilk <wielki.wilq@gmail.com>
2018-06-23 22:40:03 +0200

    change post_list.html to one that uses a class-based view using a Django generic class

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,32 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+from django.views.generic import ListView
 from .models import Post
 
 # Create your views here.
-def post_list(request):
-    object_list = Post.published.all()
-    # let's set 3 posts per page
-    paginator = Paginator(object_list, 5)
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # if page is not an integer, deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # if page is out of range, deliver last page of results
-        posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html',
-                  {'page': page, 'posts': posts})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+# THE CODE BELOW IS EQUAL TO PostListView CLASS
+# def post_list(request):
+#     object_list = Post.published.all()
+#     # let's set 3 posts per page
+#     paginator = Paginator(object_list, 5)
+#     page = request.GET.get('page')
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         # if page is not an integer, deliver the first page
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # if page is out of range, deliver last page of results
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request, 'blog/post/list.html',
+#                   {'page': page, 'posts': posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
