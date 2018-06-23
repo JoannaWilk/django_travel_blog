6876300dc6c10e4dd831706db124b85e8620e3ac
JoannaWilk <wielki.wilq@gmail.com>
2018-06-23 22:27:39 +0200

    add structural changes
    * move about.html to blog app
    * remove sitepages app
    * add pagination

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = "blog"
 
 urlpatterns = [
     url(r'^$', views.post_list, name="post_list"),
+    url(r'^about', views.about, name="about"),
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$',
         views.post_detail, name="post_detail"),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post
 
 # Create your views here.
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    object_list = Post.published.all()
+    # let's set 3 posts per page
+    paginator = Paginator(object_list, 5)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # if page is not an integer, deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if page is out of range, deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html',
+                  {'page': page, 'posts': posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
@@ -15,3 +28,6 @@ def post_detail(request, year, month, day, post):
                              publish_date__month=month,
                              publish_date__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+def about(request):
+    return render(request, 'blog/sitepage/about.html')
