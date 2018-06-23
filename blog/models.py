c02d03f32460b2af2bd692639a80f782cd9b435e
JoannaWilk <wielki.wilq@gmail.com>
2018-06-23 21:18:53 +0200

    change app logic for blog posts
    * create base.html as a template for the whole blog website
    * create SEO-friendly URLs generator for blog posts
    * change homepage to one that uses base.html
    * create template for creating blog posts
    * remove old posts application

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 from django.utils import timezone
+
+# creating custom manager
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 class Post(models.Model):
 
@@ -15,12 +21,25 @@ class Post(models.Model):
                                related_name='blog_posts',
                                on_delete=models.CASCADE)
     body = models.TextField()
+    image = models.ImageField(upload_to='media/',
+                              blank=True, null=True)
     publish_date = models.DateTimeField(default=timezone.now)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10,
                               choices=STATUS_CHOICES,
                               default='draft')
+
+    # adding created PublishedManager
+    objects = models.Manager() # default
+    published = PublishedManager()
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish_date.year,
+                             self.publish_date.strftime('%m'),
+                             self.publish_date.strftime('%d'),
+                             self.slug])
 
     class Meta:
         ordering = ('-publish_date',)
