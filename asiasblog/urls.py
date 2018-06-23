7297373c804610eb6c2bb198a8f10b56176cb78b
JoannaWilk <wielki.wilq@gmail.com>
2018-06-23 22:59:29 +0200

    change URL patterns

--- asiasblog/urls.py
+++ asiasblog/urls.py
@@ -3,8 +3,11 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 
+from blog import views
+
 urlpatterns = [
     url(r'^thisisfreakingvirus/', admin.site.urls),
-    url(r'^blog/', include('blog.urls',
-                           namespace='blog')),
+    # url(r'^$', views.post_list, name="post_list"),
+    url(r'^$', views.PostListView.as_view(), name="home"),
+    url(r'^blog/', include('blog.urls', namespace="blog")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- blog/urls.py
+++ blog/urls.py
@@ -6,8 +6,6 @@ from django.conf import settings
 app_name = "blog"
 
 urlpatterns = [
-    # url(r'^$', views.post_list, name="post_list"),
-    url(r'^$', views.PostListView.as_view(), name="post_list"),
     url(r'^about', views.about, name="about"),
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$',
         views.post_detail, name="post_detail"),
